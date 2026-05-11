b21cdd1e57b1696cf230471ac0a530338f801b39
Diego Cabrejas Azagra <do-diego@mw6f6fj9d3.powerhub>
2026-05-11 11:34:14 +0100

    add debugging code

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,6 +4,13 @@ function Connection() {
   this.pool = null;
 
   this.init = function() {
+    console.log('DB environment values:');
+    console.log('  DB_HOST =', process.env.DB_HOST);
+    console.log('  DB_PORT =', process.env.DB_PORT);
+    console.log('  DB_USER =', process.env.DB_USER);
+    console.log('  DB_NAME =', process.env.DB_NAME);
+    console.log('  DB_PASSWORD is set =', !!process.env.DB_PASSWORD);
+
     this.pool = mysql.createPool({
       connectionLimit: 10,
       host: process.env.DB_HOST || 'localhost',
@@ -16,9 +23,14 @@ function Connection() {
 
   this.acquire = function(callback) {
     this.pool.getConnection(function(err, connection) {
+      if (err) {
+        console.error('MySQL pool getConnection error:', err);
+      }
       callback(err, connection);
     });
   };
 }
 
 module.exports = new Connection();
+
+
